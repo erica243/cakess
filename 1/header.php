105e896a255af3a1e09716810b053ca2efbbb0da
erica243 <erica204chavez@gmail.com>
2024-12-09 17:04:26 +0800

    1

--- 1/header.php
+++ 1/header.php
@@ -1,5 +1,5 @@
 <?php header("Strict-Transport-Security: max-age=31536000; includeSubDomains; preload");
-
+header("X-Frame-Options: SAMEORIGIN");
 header("X-Content-Type-Options: nosniff");
 
 header("Referrer-Policy: no-referrer");
